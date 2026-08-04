ed6d0407fbbe55645e6a97f34f6ac4fbaddd0613
Julie Anne Wear <ja.wear@pm.me>
2026-08-04 14:58:43 -0700

    add login dto and users.service

---
+++ api/src/app/modules/users/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOne({
+      where: { email },
+      select: { id: true, email: true, password: true },
+    });
+  }
+}
