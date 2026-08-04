8182e197355c146339a1f2a153a5374b8ddcb753
Julie Anne Wear <ja.wear@pm.me>
2026-08-04 15:30:42 -0700

    add jwt package and login method

--- api/src/app/modules/auth/auth.service.ts
+++ api/src/app/modules/auth/auth.service.ts
@@ -1,16 +1,32 @@
 import * as bcrypt from 'bcrypt';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { UsersService } from '../users/users.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signup(
     createUserDto: CreateUserDto,
   ): Promise<{ id: string; email: string }> {
     const user = await this.usersService.create(createUserDto);
     return { id: user.id, email: user.email };
+  }
+
+  async login(loginDto: LoginDto) {
+    const user = await this.usersService.findByEmail(loginDto.email);
+
+    if (!user || !(await bcrypt.compare(loginDto.password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { sub: user.id, email: user.email };
+    return { accessToken: this.jwtService.sign(payload) };
   }
 }
